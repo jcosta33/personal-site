5e38c3c8c81190cff332d40cf6b315d8c2e020f3
José Costa <josecosta@Joses-Mac-mini.local>
2021-05-03 20:22:03 +0100

    updated tasks

--- gulpfile.js
+++ gulpfile.js
@@ -136,6 +136,10 @@ gulp.task("compress-images", () =>
   ]))
 );
 
+gulp.task("move-public", () =>
+  gulp.src(['src/public/*']).pipe(gulp.dest('dist'))
+);
+
 gulp.task("move-images", () =>
   gulp.src(['src/images/**/*']).pipe(gulp.dest('dist/images'))
 );
@@ -160,6 +164,6 @@ gulp.task("watch", () => {
 // Gulp tasks
 gulp.task("serve", gulp.parallel("browser-sync", "watch"));
 gulp.task("merge", gulp.series("concat-styles", "concat-js")); // Merge & minify css + js
-gulp.task("build", gulp.series("nunjucks", "styles", "merge", "move-images", "move-fonts")); // Compile sass, concat and minify css + js
+gulp.task("build", gulp.series("nunjucks", "styles", "merge", "move-images", "move-fonts", "move-public")); // Compile sass, concat and minify css + js
 gulp.task("lint", gulp.series("lint-styles", "lint-scripts")); // Lint css + js files
 gulp.task("default", gulp.series("build", "serve")); // Default gulp task
